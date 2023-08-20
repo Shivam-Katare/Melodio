a9f7be14c8bb73d1517f35cbca4e3a0fa0bf8da5
Shivam-Katare <katare27451@gmail.com>
2023-08-20 20:16:48 +0530

    All codes are updated.

---
+++ ReadME.md
@@ -0,0 +1,42 @@
+# Melodio - Your Open Source Music App
+
+
+
+Melodio is a delightful open-source music app that allows you to curate your music experience. With Melodio, you have the freedom to add your favorite tracks and create a personalized playlist that resonates with your musical tastes.
+
+## Key Features
+
+- **Play and Pause:** Enjoy seamless control over your music playback with the play and pause buttons.
+- **Previous and Next:** Navigate through your playlist effortlessly with the previous and next track buttons.
+- **Draggable Progress Bar:** Easily seek to any part of a song by dragging the progress bar forward or backward.
+- **Background Animation:** Experience the rhythm visually with dynamic background color animation synchronized to the music.
+- **Loop and Shuffle:** Choose between looping a song, shuffling your playlist, or repeating the entire playlist.
+- **Custom Songs:** Melodio provides the flexibility to add your personal collection of songs for a tailored listening experience.
+
+## How to Contribute
+
+We encourage contributions from the community to make Melodio even better. Follow these steps to contribute:
+
+1. **Fork the Repository:** Start by forking the Melodio repository on GitHub.
+2. **Clone the Repository:** Clone your forked repository to your local machine.
+3. **Start Server:** Visit the page locally on your machine.
+4. **Create a new branch:** Before making any changes, create a new branch for your contribution.
+5. **Make Changes:** Make your desired improvements, bug fixes, new features or even a **new song**.
+6. **Test:** Run the app and ensure that your changes work as expected.
+7. **Commit and Push:** Commit your changes and push them to your forked repository.
+8. **Create Pull Request:** Open a pull request against the original Melodio repository.
+
+
+## Getting Started
+
+To start using Melodio, simply follow these steps:
+
+1. Clone or download the repository.
+2. Open the `index.html` file in your preferred web browser.
+3. Explore your music collection, play your tracks, and enjoy!
+
+
+
+---
+
+Feel the rhythm, discover new beats, and enjoy music your way with Melodio!
---
+++ music-list.js
@@ -0,0 +1,62 @@
+let allMusic = [
+  {
+    name: "Always with Me",
+    artist: "Daddy_s_music",
+    img: "https://cdn.pixabay.com/photo/2017/06/20/22/14/man-2425121_1280.jpg",
+    src: "https://cdn.pixabay.com/audio/2022/02/18/audio_bf80c561fe.mp3"
+  },
+  {
+    name: "Ambient Classical Guitar",
+    artist: "William_King",
+    img: "https://cdn.pixabay.com/audio/2023/04/03/14-44-18-281_200x200.png",
+    src: "https://cdn.pixabay.com/audio/2023/04/03/audio_6700efb297.mp3"
+  },
+  {
+    name: "The Cradle of Your Soul",
+    artist: "lemonmusicstudio",
+    img: "https://cdn.pixabay.com/audio/2022/04/06/06-57-42-712_200x200.png",
+    src: "https://cdn.pixabay.com/audio/2022/01/26/audio_d0c6ff1bdd.mp3"
+  },
+  {
+    name: "Happy Acoustic Guitar Background Music",
+    artist: "Music_Unlimited",
+    img: "https://cdn.pixabay.com/audio/2022/10/12/10-43-43-53_200x200.jpg",
+    src: "https://cdn.pixabay.com/audio/2022/10/12/audio_2eca99b967.mp3"
+  },
+  {
+    name: "Sleepy cat",
+    artist: "Lofi_hour",
+    img: "https://cdn.pixabay.com/audio/2022/09/02/21-41-57-430_200x200.jpg",
+    src: "https://cdn.pixabay.com/audio/2022/09/02/audio_4dd705d8e5.mp3"
+  },
+  {
+    name: "Cinematic Fairy Tale Story",
+    artist: "Daddy_s_Music",
+    img: "https://cdn.pixabay.com/photo/2017/06/20/22/14/man-2425121_1280.jpg",
+    src: "https://cdn.pixabay.com/audio/2021/09/25/audio_153f263349.mp3"
+  },
+  {
+    name: "Relaxed Vlog (Night Street)",
+    artist: "Ashot-Danielyan-Composer",
+    img: "https://cdn.pixabay.com/audio/2023/05/04/11-36-38-58_200x200.jpg",
+    src: "https://cdn.pixabay.com/audio/2023/01/01/audio_816821e627.mp3"
+  },
+  {
+    name: "Summer Walk",
+    artist: "Olexy",
+    img: "https://cdn.pixabay.com/photo/2017/06/20/22/14/man-2425121_1280.jpg",
+    src: "https://cdn.pixabay.com/audio/2023/06/06/audio_cbcfcb18e6.mp3"
+  },
+  {
+    name: "Happy Day",
+    artist: "Top-Flow-Production",
+    img: "https://cdn.pixabay.com/audio/2023/04/30/12-30-33-725_200x200.png",
+    src: "https://cdn.pixabay.com/audio/2023/05/11/audio_5198c12188.mp3"
+  },
+  {
+    name: "Epicaly",
+    artist: "LiteSaturation",
+    img: "https://cdn.pixabay.com/audio/2022/06/25/14-55-33-454_200x200.jpg",
+    src: "https://cdn.pixabay.com/audio/2022/06/25/audio_944aab53cf.mp3"
+  }
+];
